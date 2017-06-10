dbe37525253e01e0e23c67dbb7837e4425ed6f4a
Mohammad Al-Abbasi <sheikhhmo@gmail.com>
2017-06-10 18:09:09 -0400

    second commit for progress branch

--- progress.model.js
+++ progress.model.js
@@ -1,40 +1,70 @@
-//to store each users nutrients and amounts
-var nutrients = {};
-//user entered weight
-var myWeight = 0;
 
-//add a selected nutrient and amount to users nutrients object
-function addNutr(nutr, amount){
-	if(nutr in nutrients){
-		nutrients.nutr += amount;
-	}
-	else {
-		nutrients.nutr = amount;
-	}
-};
+	var progressModel = (function(){	
+		//to store each users nutrients and amounts
+		var nutrients = {};
 
-//specify a nutrient and amount to remove from object
-function removeNutr(nutr, amount){
-	if(nutrients.nutr < amount){
-		nutrients.nutr = 0;
-	}
-	else {
-		nutrients.nutr -= amount;
-	}
-};
+		//user defined goal for nutrient intake
+		var target = {};
 
-//user defined goal for nutrient and amount
-function setGoal(nutr, amount){
-	var item = nutr;
-	var goal = amount;
-};
+		//user entered weight
+		var myWeight = 0;
 
-//user updates current weight
-function setWeight(weight){
-	myWeight = weight;
-};
+		//add a selected nutrient and amount to users nutrients object
+		function addNutr(nutr, unit, amount){
 
-//return user weight
-function getWeight(){
-	return myWeight;
-};
+			if(nutr in nutrients){
+				nutrients.nutr += amount;
+			}
+			else {
+				nutrients[nutr] = amount;
+			}
+			console.log("You have added " + amount + " " + unit + " of " + nutr + "!.")
+			console.log(nutrients);
+
+
+			//return true;
+		};
+
+		//specify a nutrient and amount to remove from object
+		function removeNutr(nutr, unit, amount){
+
+			if (nutr in nutrients){
+				if(nutrients.nutr < amount){
+					console.log("You have removed all " + nutr + "!.");
+					nutrients.nutr = 0;
+				}
+				else {
+					this.nutr -= amount;
+
+					console.log("You have removed " + amount + " " + unit + " of " + nutr + ", "
+					+ "you still have " + this.nutr + " left!.");
+					console.log(nutrients);
+				}
+			}
+			else {
+				console.log("There are none of this nutrient");
+			}
+		};
+
+		//user defined goal for nutrient and amount
+		function setGoal(nutr, amount){
+			target.nutr = amount;
+		};
+
+		//user updates current weight
+		function setWeight(weight){
+			myWeight = weight;
+		};
+
+		var listeners = [];
+
+
+
+		return {
+			addNutr: addNutr,
+			removeNutr: removeNutr,
+			setGoal: setGoal,
+			setWeight: setWeight,
+		};
+
+	})();//end model
